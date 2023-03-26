20b5f5f5506bda2df0926b6888b733abce41f795
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-03-27 00:39:24 +0800

    React Tutorial

---
+++ myproject/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello.js';
+import Message from './components/Message.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <Message/>
+    </div>
+  );
+}
+
+export default App;
