9355c7a7e174050cc36d124c5fadf51529246a66
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-03-30 00:48:12 +0800

    Conditional Rendering using Elements

--- myproject/src/components/ConditionalComponent.js
+++ myproject/src/components/ConditionalComponent.js
@@ -4,28 +4,20 @@ import FunctionalCounter from "./FunctionalCounter";
 
 export default function ConditionalComponent() {
 
-    const [display, setDisplay] = useState(true);
+    const [display, setDisplay] = useState(false);
+    let output;
 
     if (display) {
-        return (
-            <div>
-                <h3>This is a conditional component</h3>
-                <Counter/>
-            </div>
-        )
+        output = <div>
+            <h3>This is a conditional component</h3>
+            <Counter/>
+        </div>
     } else {
-        return (
-            <div>
-                <h3>Nothing to see here</h3>
-                <FunctionalCounter/>
-            </div>
-        )
+        output = <div>
+            <h3>Nothing to see here</h3>
+            <FunctionalCounter/>
+        </div>
     }
 
-    // return (
-    //     <div>
-    //         <h3>This is a conditional component</h3>
-    //         <h3>Nothing to see here</h3>
-    //     </div>
-    // )
+    return (output);
 }
