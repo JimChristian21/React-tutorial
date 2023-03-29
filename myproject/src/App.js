562c0e5c5b9925a67c076bd40f31afb81833ee78
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-03-29 22:56:50 +0800

    State in functional component

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import FunctionEvent from "./components/FunctionEvent.js";
-import ClassEvent from "./components/ClassEvent.js";
-import Counter from "./components/Counter.js";
+// import FunctionEvent from "./components/FunctionEvent.js";
+// import ClassEvent from "./components/ClassEvent.js";
+// import Counter from "./components/Counter.js";
+import FunctionalCounter from './components/FunctionalCounter.js';
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <FunctionalCounter/>
     </div>
   );
 }
