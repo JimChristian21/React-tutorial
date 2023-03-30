57dfd786cd1e84cd51a0f849f74156606670aae2
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-03-30 22:33:46 +0800

    List Object and list key

--- myproject/src/components/Product.js
+++ myproject/src/components/Product.js
@@ -1,8 +1,9 @@
 export default function Product() {
 
-    const products = ["Laptop", "Phone", "Modem"];
+    // const products = ["Laptop", "Phone", "Modem", "Computer"];
+    const products = [{id:1,name:"Laptop", price: 500},{id:2,name:"Computer", price: 100}];
     const productList = products.map((product, i) => {
-            return <h3 key={i}>{product}</h3>
+            return <h3 key={product.id}>{product.name}: ${product.price}</h3>
         })
 
     return (
