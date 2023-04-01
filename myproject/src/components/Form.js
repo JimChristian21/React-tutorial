b8d88f6d23170ca6fbf1bd390e02eccb3891e32d
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-01 23:57:12 +0800

    Added Input

--- myproject/src/components/Form.js
+++ myproject/src/components/Form.js
@@ -3,7 +3,8 @@ import {Component} from "react";
 class Form extends Component {
 
     state = {
-        firstname: "" 
+        firstname: "",
+        lastname: "",
     }
 
     handleChange = (event) => {
@@ -12,11 +13,22 @@ class Form extends Component {
         });
     }
 
+    handleLastNameChange = (event) => {
+        this.setState({
+            lastname: event.target.value
+        });
+    }
+
     render() {
         return (
             <div>
                 <form>
+                    <label>First Name: </label>
                     <input onChange={this.handleChange} type="text" value={this.state.firstname}/>
+                    <br/>
+                    <br/>
+                    <label>Last Name: </label>
+                    <input onChange={this.handleLastNameChange} type="text" value={this.state.lastname}/>
                 </form>
             </div>
         )
