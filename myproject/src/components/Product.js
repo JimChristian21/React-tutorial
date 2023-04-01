11e50c10cadac3ebab484bf190caffe5dfc12bec
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2023-04-01 16:38:31 +0800

    Rendering Array

--- myproject/src/components/Product.js
+++ myproject/src/components/Product.js
@@ -1,14 +1,20 @@
 export default function Product() {
 
     // const products = ["Laptop", "Phone", "Modem", "Computer"];
-    const products = [{id:1,name:"Laptop", price: 500},{id:2,name:"Computer", price: 100}];
-    const productList = products.map((product, i) => {
-            return <h3 key={product.id}>{product.name}: ${product.price}</h3>
+    const products = [
+        {id:1,name:"Laptop", price: 500},
+        {id:2,name:"Computer", price: 100}
+    ];
+
+    const fruits = ['Apple', 'Mango', 'Banana'];
+
+    const fruitList = fruits.map((fruit, i) => {
+            return <h3 key={i}>{fruit}</h3>
         })
 
     return (
         <div>
-            {productList}
+            {fruitList}
         </div>
     );
 }
